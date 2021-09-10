75972523f55cff526b166b1221fbb6542a52f9ef
andriwskenda <skenda.computers@gmail.com>
2021-09-10 11:21:54 -0500

    se crearon las paginas por modulos de la mision vision y quienes somos tratados como modulos

--- code/AgSoftware/Prueba2/registration.php
+++ code/AgSoftware/Prueba2/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AgSoftware_Prueba2', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Agsoftware_Prueba2', __DIR__);
